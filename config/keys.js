3619a16875c24d282916632ec2bb5deeac8174b5
dokyan1989 <38043228+dokyan1989@users.noreply.github.com>
2018-11-06 07:24:34 +0700

    Add User API Routes and JWT Authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://dokyan1989:abCD12--@ds123929.mlab.com:23929/devconnector'
-}
+  mongoURI:
+    'mongodb://dokyan1989:abCD12--@ds123929.mlab.com:23929/devconnector',
+  secretOrKey: 'secret'
+};
